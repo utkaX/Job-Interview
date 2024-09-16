d42f52a75d3e3930ffb7ec778c240616e3c82bd7
karangurjar16 <gurjarkaran03022004@gmail.com>
2024-09-16 23:17:04 +0530

    some part  of the employer add done

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -4,7 +4,8 @@ import Dashboard from './components/Home/Dashboard';
 import Signup from "./auth/signup";
 import Login from "./Auth/login";
 import OtpVerification from "./auth/verifyOtp";
-import AddProfile from "./components/AddProfile";
+import AddProfile from "./components/Jobseeker/AddProfile";
+import CompanyProfile from "./components/Employer/CompanyProfile"
 
 function App() {
   // console.log(AddProfile);
@@ -13,10 +14,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/add-profile" element={<AddProfile />} />
+        <Route path="/company-profile" element={<CompanyProfile />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/verify-otp" element={<OtpVerification />} />
         <Route path="/login" element={<Login />} />
-        {/* Add more routes here */}
       </Routes>
     </Router>
   );
