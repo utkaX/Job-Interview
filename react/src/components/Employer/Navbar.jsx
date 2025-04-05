0167db16d6e51d0540a8de8ba17df358a11c762b
Karan Gurjar <142745773+karangurjar16@users.noreply.github.com>
2025-04-05 16:48:08 +0530

    Reapply "images bug fixed"
    This reverts commit 420a9f83e8f5ad131320a079a79bc69c6bfea8ca.

--- react/src/components/Employer/Navbar.jsx
+++ react/src/components/Employer/Navbar.jsx
@@ -1,10 +1,10 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useState, useEffect, useRef } from "react";
 import { FaBell, FaBookmark } from "react-icons/fa"; // Notification and Saved Jobs icons
-import Logo from "../../common/Logo.png"; // Replace with your actual logo path
 import { useAuth } from "../../context/authContext";
 import config from "../../utils/config";
 export default function EmployerNavbar() {
+  const Logo = "https://i.imgur.com/4Q0v1gM.png"; // Replace with your actual logo path
   const { isLoggedIn, logout, user } = useAuth(); // Get the user and logout from auth context
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [notificationCount, setNotificationCount] = useState(0); // State for notification count
